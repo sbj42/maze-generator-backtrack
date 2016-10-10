38e0b90d986b195f58dd3d527a75267f8d4c79f4
James Clark <sbj@dimins.com>
2016-10-09 21:36:44 -0400

    convert to use new -dev library

--- test/test-backtrack.js
+++
@@ -1,5 +0,0 @@
-var testAlgorithm = require('@sbj42/maze-generator-support/test-support/test-algorithm');
-
-var backtrack = require('../src/backtrack');
-
-testAlgorithm('maze-generator-backtrack', backtrack);
